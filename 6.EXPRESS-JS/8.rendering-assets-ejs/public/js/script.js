144130d47b1c9c96d6b289824d61a5f3e4e15c22
charubabbar6 <charubabbar6@gmail.com>
2024-09-10 21:43:50 +0100

    test

--- 6.EXPRESS-JS/8.rendering-assets-ejs/public/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector("h1");
-  header.addEventListener("click", () => {
-    header.style.color = "yellow";
-  });
-});
